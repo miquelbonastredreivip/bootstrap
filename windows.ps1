4ea8dcc167c1b054a3dfc2510ac98cc744ca4a3c
miquelbonastredreivip <61009812+miquelbonastredreivip@users.noreply.github.com>
2020-03-25 14:31:16 +0100

    Convert from bat to powershell

---
+++ windows.ps1
@@ -0,0 +1,5 @@
+$INSTALLER_URL = "https://repo.saltstack.com/windows/Salt-Minion-3000-Py2-x86-Setup.exe"
+$MASTER_IP     = "10.20.2.2"
+
+Add-Content C:\Windows\system32\drivers\etc\hosts "$MASTER_IP salt"
+Invoke-Expression ((new-object net.webclient).DownloadString("$INSTALLER_URL"))
