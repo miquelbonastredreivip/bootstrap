d07a8afb986fc9b7c1093a8bf5696587f454f781
Miquel Bonastre <miquel.bonastre@dreivip.com>
2021-05-12 15:28:07 +0200

    Solve errors in macos.sh

--- macos.sh
+++ macos.sh
@@ -24,37 +24,29 @@ echo "  user='${ADM_USER}' salt='${SALT_HOST}' minion_id='${THIS_HOST}'"
 echo "(Enter to continue, Ctrl-C to exit)"
 read line
 
-#
-# enable SSH access
-#
+echo "Enabling SSH access:"
 sudo systemsetup -setremotelogin on
 sudo dseditgroup -o create -q com.apple.access_ssh
 sudo dseditgroup -o edit -a "${ADM_USER}" -t user com.apple.access_ssh
 
-#
-# add salt server to host file
-#
-if ! grep -q -e "^${SALT}  *salt *$" ; then
-  sudo bash -c "echo '${SALT} salt' >> /etc/hosts"
+echo "Adding salt server to host file:"
+
+if ! grep -q -e "^${SALT_HOST}  *salt *$" /etc/hosts ; then
+  sudo bash -c "echo '${SALT_HOST} salt' >> /etc/hosts"
 fi
 
-#
-# Install homebrew
-#
+echo "Installing homebrew:"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-#
-# Install and launch salt-minion
-#
 
-#
-# ("brew install salt" has problems)
+# ("brew install salt" has some problems)
 #
 # brew install salt
 # sudo salt-minion -d
 
-# Install saltstack using bootstrap
-# (https://repo.saltproject.io/)
+echo "Installing and launching salt-minion:"
+echo "(using bootstrap)"
+
 curl -fsSL https://bootstrap.saltproject.io -o install_salt.sh
 sudo sh install_salt.sh -P -x python3
 
